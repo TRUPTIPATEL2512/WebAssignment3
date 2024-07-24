a54a98b7f20b502eb85b80cf72f5ff591929fa87
TRUPTIPATEL2512 <156118870+TRUPTIPATEL2512@users.noreply.github.com>
2024-07-24 16:36:54 -0400

    Setup the environment

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace A3_TruptiPatel.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
